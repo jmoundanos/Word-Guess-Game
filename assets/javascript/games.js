b5ca9acfc303b14cc3184121aebcbc1a273ba934
Jennifer Moundanos <jmoundanos@gmail.com>
2019-12-20 20:03:37 -0600

    changed layout and started js

---
+++ assets/javascript/games.js
@@ -0,0 +1,15 @@
+
+var wordsToGuess = ['walk', 'trot', 'canter', 'gallop', 'lope', 'halt', 
+                'saddle', 'bridle', 'bit', 'dressage', 'hunter', 'jumper',
+                'english', 'western', 'tack', 'jog', 'eventing', 'neigh',
+                'nicker', 'hoof', 'mane', 'tail', 'forelock', 'whinny', 'halter']
+var letterGuessed = [];
+var wins = 0;
+var guessCounter = 0;
+var playAgain = false;
+var guessingWord = [];
+    
+    document.onkeyup = function(){
+        alert("key pressed");
+    }
+
