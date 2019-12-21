e024f507d9f84310627ea5ef34abaeccfd6112c2
Jennifer Moundanos <jmoundanos@gmail.com>
2019-12-20 22:44:24 -0600

    created loops to choose random word

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -3,13 +3,25 @@ var wordsToGuess = ['walk', 'trot', 'canter', 'gallop', 'lope', 'halt',
                 'saddle', 'bridle', 'bit', 'dressage', 'hunter', 'jumper',
                 'english', 'western', 'tack', 'jog', 'eventing', 'neigh',
                 'nicker', 'hoof', 'mane', 'tail', 'forelock', 'whinny', 'halter']
-var letterGuessed = [];
+var lettersGuessed = [];
 var wins = 0;
 var guessCounter = 0;
 var playAgain = false;
-var guessingWord = [];
+var randomWord = "";
+var wordLength = [];
     
+//start game
     document.onkeyup = function(){
-        alert("key pressed");
+        //choose word from array 
+    randomWord = wordsToGuess[Math.floor(Math.random() * wordsToGuess.length)];
+    //fill array with underscores based on length of word
+    for(var i = 0; i < randomWord.length; i++){
+        wordLength[i] = "_";
+        document.getElementById("wordToGuess").innerHTML = wordLength.join(" ");
+    }
+    //start game loop for number of guesses user is allowed
+    if(guessCounter < 16){
+        
+    }
     }
 
