f449b84a42eae336bccc12394dbec357c008c663
Jennifer Moundanos <jmoundanos@gmail.com>
2019-12-27 16:42:22 -0600

    fixed js to start and play game

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -1,27 +1,55 @@
-
 var wordsToGuess = ['walk', 'trot', 'canter', 'gallop', 'lope', 'halt', 
                 'saddle', 'bridle', 'bit', 'dressage', 'hunter', 'jumper',
                 'english', 'western', 'tack', 'jog', 'eventing', 'neigh',
                 'nicker', 'hoof', 'mane', 'tail', 'forelock', 'whinny', 'halter']
-var lettersGuessed = [];
-var wins = 0;
-var guessCounter = 0;
-var playAgain = false;
-var randomWord = "";
-var wordLength = [];
-    
-//start game
-    document.onkeyup = function(){
-        //choose word from array 
-    randomWord = wordsToGuess[Math.floor(Math.random() * wordsToGuess.length)];
-    //fill array with underscores based on length of word
-    for(var i = 0; i < randomWord.length; i++){
-        wordLength[i] = "_";
-        document.getElementById("wordToGuess").innerHTML = wordLength.join(" ");
-    }
-    //start game loop for number of guesses user is allowed
-    if(guessCounter < 16){
-        
-    }
-    }
 
+var wins = 0;
+var guessCounter = 15;
+var randomWord = "";
+var letterArray = [];
+var spaces = [];
+var spacesAndCorrectLetter = [];
+var wrongLetter = [];
+
+document.onkeyup = function(){
+    //select random word
+    randomWord = wordsToGuess[Math.floor(Math.random() * wordsToGuess.length)];
+    letterArray = randomWord.split("");
+    spaces = letterArray.length;
+    //fill array with underscores based on length of word
+    for(var i = 0; i < spaces; i++){
+        spacesAndCorrectLetter.push("_");
+        }
+    document.getElementById("wordToGuess").innerHTML = "  " + spacesAndCorrectLetter.join(" ");
+    console.log(randomWord);
+
+document.onkeyup = function(event){
+    //guessCounter++;
+    var letterGuess = event.key;
+    var letter = false;
+    for(var j = 0; j < spaces; j++){
+        if(randomWord[j] == letterGuess){
+            letter = true;
+        }
+    }
+    if(letter){
+        for(var k = 0; k < spaces; k++){
+            if(randomWord[k] == letterGuess){
+                spacesAndCorrectLetter[k] = letterGuess;
+                guessCounter--;
+                document.getElementById("wordToGuess").innerHTML = spacesAndCorrectLetter.join("");
+            }
+        }
+
+    }else{
+        wrongLetter.push(letterGuess);
+        document.getElementById("guessedLetters").innerHTML = wrongLetter.join("");
+        guessCounter--;
+     }
+}
+    if(letterArray.toString() == spacesAndCorrectLetter.toString()){
+        wins++;
+        document.getElementById("wins").innerHTML = wins;
+        document.getElementById("congrats").innerHTML = "You win!";
+    }
+}
